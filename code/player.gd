805685d0bba5f12f3745315edde3b1027526c657
Gabriel Miller <gabrielmiller@AscensionAcademy.local>
2024-05-03 09:25:20 -0500

    "i added files"

---
+++ code/player.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var speed: int = 100
+var velocity = Vector2.ZERO
+var screensize = Vector2(1152, 648)
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.		
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	position += velocity * speed * delta
